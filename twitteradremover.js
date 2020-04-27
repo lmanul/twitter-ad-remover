0dc21d94bee70cebfa11b7579e93e515252cd5c3
Manu Cornet <m@ma.nu>
2020-04-27 01:07:10 +0000

    Be a little more resilient to in-app navigation, and actually hide ads now

--- twitteradremover.js
+++ twitteradremover.js
@@ -1,24 +1,43 @@
+let counter = 0;
 
 function findTweets() {
   // Get the time line.
-  let list = document.querySelector('section[aria-labelledby="accessible-list-0"]');
+  let list = document.getElementsByTagName('section')[0];
+
+  if (list.length < 1) {
+    // Maybe the content hasn't loaded yet, do some polling.
+    window.setTimeout(findTweets, 2000);
+    return;
+  }
+
   // The first child is a 'h1'.
-  if (list.childNodes.length < 2) {
+  if (!list || !list.childNodes || list.childNodes.length < 2) {
     return [];
   }
-  return list.childNodes[1].firstChild.childNodes;
+
+  let sectionSecondChild = list.childNodes[1];
+  if (!sectionSecondChild || !sectionSecondChild.firstChild) {
+    return [];
+  }
+  return sectionSecondChild.firstChild.childNodes;
 }
 
-var observer = new MutationObserver(function(changes) {
+function removeAds(changes) {
   let tweets = findTweets();
   for (let i = 0; i < tweets.length; i++) {
     if (tweets[i].innerHTML.indexOf('Promoted') != -1) {
-      tweets[i].style.backgroundColor = 'red';
+      console.log('' + counter++ + ' ads have been hidden so far.');
+      tweets[i].style.display = 'none';
     }
   }
+}
+
+var observer = new MutationObserver(function(changes) {
+  removeAds();
 });
 
-let target = document.querySelector('body');
+let target = document.querySelector('div#react-root');
+
 observer.observe(target, {
   subtree: true,
   childList: true
